ffe4730738e0de859161d1b9ea67b1b3bf440bdd
Alex <ronaldalexandervacca@unicesar.edu.co>
2022-09-19 18:58:02 -0500

    Inicio del Proyecto Gestion de _Notas

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+@app.route('/botones')
+def botones():
+    return render_template('botones.html')
+
+
+if  __name__=='__main__':
+     app.run(debug=True)  
